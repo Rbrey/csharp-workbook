97624de0aff728f7166004f7968f33b9d59571ff
Rbrey <35275423+Rbrey@users.noreply.github.com>
2018-04-16 00:23:41 -0500

    Pig Latin

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -7,7 +7,10 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
-
+            string word = "";
+            Console.WriteLine("What do you need translated?");
+            word = Console.ReadLine();
+            Console.WriteLine(TranslateWord(word));
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
@@ -15,6 +18,16 @@ namespace PigLatin
         public static string TranslateWord(string word)
         {
             // your code goes here
+            string[] words = word.Split(' ');
+            word = string.Empty;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= 1) continue;
+                string output = new string(words[i].ToCharArray());
+                output = output.Substring(1, output.Length -1) + output.Substring(0, 1) + "ay";
+                word += output;
+            }
+
             return word;
         }
     }
